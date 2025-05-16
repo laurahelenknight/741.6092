5d31f7db2eb9a03837498d0f3bfc80aa0dd1369b
laurahelenknight <laura.h.knight@gmail.com>
2025-05-16 16:13:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -149,7 +149,7 @@ const DesignerGallery = () => {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-6">741.6092: Graphic Designers</h1>
+      <h1 className="text-4xl font-bold mb-6">741.6092: Graphic Designers</h1>
       
       <div className="mb-8">
   {/* Filter Groups */}
